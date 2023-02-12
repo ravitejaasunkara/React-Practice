0d61e74b33603e8bb785f176cde5d8ce040248cd
Raviteja Sunkara <101201768+ravitejaasunkara@users.noreply.github.com>
2023-02-12 19:07:10 +0530

    now useReduce

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./styles.css";
 import { useEffect, useState } from "react";
 export default function App() {
-  const [value, setValue] = useState(0);
-  const [value1, setValue1] = useState(0);
-  useEffect(() => {
-    console.log("Use Effect");
-  }, [value]);
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2 onClick={() => setValue(value + 1)}>this is value1</h2>
-      <h2 onClick={() => setValue1(value1 + 1)}>this is value2</h2>
-      <p>value1 : {value}</p>
-      <p>value2 : {value1}</p>
-    </div>
-  );
+  // const [value, setValue] = useState(0);
+  // const [value1, setValue1] = useState(0);
+  // useEffect(() => {
+  //   console.log("Use Effect");
+  // }, [value]);
+  // return (
+  //   <div className="App">
+  //     <h1>Hello CodeSandbox</h1>
+  //     <h2 onClick={() => setValue(value + 1)}>this is value1</h2>
+  //     <h2 onClick={() => setValue1(value1 + 1)}>this is value2</h2>
+  //     <p>value1 : {value}</p>
+  //     <p>value2 : {value1}</p>
+  //     <p>Hello</p>
+  //   </div>
+  // );
 }
