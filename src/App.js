5bf733d030e80a2abd4077b391567c303404088c
Raviteja Sunkara <101201768+ravitejaasunkara@users.noreply.github.com>
2023-02-23 13:20:53 +0530

    useReducer example

--- src/App.js
+++ src/App.js
@@ -1,62 +1,41 @@
-import "./styles.css";
-import { useEffect, useState, useReducer } from "react";
+import React, { useReducer, useState } from "react";
+const initialValue = [];
 const ACTIONS = {
-  INCREMENT:'Increment',
-  DECREMENT:'Decrement'
-}
-const reducer = (state,action) => {
-  switch(action.type){
+  INCREMENT: "add"
+};
+const reducerFn = (state, action) => {
+  switch (action.type) {
     case ACTIONS.INCREMENT:
-      return {count:state.count+1};
-    case ACTIONS.DECREMENT:
-      return {count:state.count-1};
+      return [...state, newTodo(action.payload.name)];
     default:
-      return state.count;
+      return state;
   }
 };
-export default function App() {
-  // const [value, setValue] = useState(0);
-  // const [value1, setValue1] = useState(0);
-  // useEffect(() => {
-  //   console.log("Use Effect");
-  // }, [value]);
-  // return (
-  //   <div className="App">
-  //     <h1>Hello CodeSandbox</h1>
-  //     <h2 onClick={() => setValue(value + 1)}>this is value1</h2>
-  //     <h2 onClick={() => setValue1(value1 + 1)}>this is value2</h2>
-  //     <p>value1 : {value}</p>
-  //     <p>value2 : {value1}</p>
-  //     <p>Hello</p>
-  //   </div>
-  // );
-  // const [count, setCount] = useState(0);
-  // const Increment = () => {
-  //   setCount((prevState) => prevState + 1);
-  // };
-  // const Decrement = () => {
-  //   setCount((prevState) => prevState - 1);
-  // };
-  // return (
-  //   <div>
-  //     <h2>Hello useReduce</h2>
-  //     <button onClick={Decrement}>Decrement</button>
-  //     <h3>{count}</h3>
-  //     <button onClick={Increment}>Increment</button>
-  //   </div>
-  // );
-  const [state,dispatch] = useReducer(reducer,{count:0});
-  const Increment = () => {
-    dispatch({type:ACTIONS.INCREMENT});
+const newTodo = (name) => {
+  return { name: name, id: Math.random().toFixed(0) * 10 };
+};
+const App = () => {
+  const [name, setName] = useState("");
+  const [todos, dispatch] = useReducer(reducerFn, initialValue);
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch({ type: ACTIONS.INCREMENT, payload: { name: name } });
+    setName("");
   };
-  const Decrement = ()  => {
-    dispatch({type:ACTIONS.DECREMENT});
-  };
-  return(
+  console.log(todos);
+  return (
     <div>
-      <button onClick={Increment}>Increment</button>
-      <p>{state.count}</p>
-      <button onClick={Decrement}>Decrement</button>
+      <form onSubmit={submitHandler}>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </form>
+      {todos.map((item) => {
+        return <li key={item.id}>{item.name}</li>;
+      })}
     </div>
   );
-}
+};
+export default App;
